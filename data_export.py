dec64e74416ab28930542291e58bb689c3d085ab
Martin Aumueller <maau@itu.dk>
2021-02-26 10:25:50 +0100

    updated experiment script

--- data_export.py
+++ data_export.py
@@ -11,8 +11,11 @@ def get_all_results():
 if __name__ == "__main__":
     for f in get_all_results():
         g = h5py.File(os.path.join("data", f"{f.attrs['dataset']}.hdf5"), 'r')
-        abs_err = np.mean(np.abs(np.array(f['errors']) - np.array(g['kde.001'])))
-        query_time = np.mean(f['times'])
-        print(f'{f.attrs["algorithm"]}\t{abs_err}\t{query_time}')
+        try:
+            abs_err = np.mean(np.abs(np.array(f['errors']) - np.array(g['kde.01']))/np.array(g['kde.01']))
+            query_time = np.mean(f['times'])
+            print(f'{f.attrs["algorithm"]}\t{abs_err}\t{query_time}')
+        except:
+            print(f"Couldn't process {f}")
 
 
